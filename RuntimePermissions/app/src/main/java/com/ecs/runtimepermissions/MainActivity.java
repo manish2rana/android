c82cfca7eb181f7219da3cf1f7749a3a40e3be4c
manish2rana <manishrana366@gmail.com>
2017-12-27 18:49:16 +0530

    Permission to Read Contact
    Added Permission to Read a single Contact from System Contact application.

--- RuntimePermissions/app/src/main/java/com/ecs/runtimepermissions/MainActivity.java
+++ RuntimePermissions/app/src/main/java/com/ecs/runtimepermissions/MainActivity.java
@@ -3,9 +3,11 @@ package com.ecs.runtimepermissions;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,12 +16,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CAMERA_PERMISSION = 100;
+    public static final int OPEN_CAMERA_PERMISSION = 100;
+    public static final int READ_CONTACT_PERMISSION = 101;
+
     public static final int REQUEST_IMAGE_CAPTURE = 200;
+    public static final int REQUEST_READ_CONTACT = 201;
 
 
     @Override
@@ -29,32 +33,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void captureCameraImage(View view) {
-
         int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
             boolean flag = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA);
             if (flag) {
                 openCamera();
             } else {
-                Toast.makeText(this, "per", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "OPEN CAMERA PERMISSION ALREADY ENABLED", Toast.LENGTH_SHORT).show();
                 ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.CAMERA},
-                        CAMERA_PERMISSION);
+                        OPEN_CAMERA_PERMISSION);
             }
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, OPEN_CAMERA_PERMISSION);
         }
     }
+
+
+    public void readContact(View view) {
+        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            boolean flag = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS);
+            if (flag) {
+                getContacts();
+            } else {
+                Toast.makeText(this, "READ CONTACTS PERMISSION ALREADY ENABLED", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        READ_CONTACT_PERMISSION);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACT_PERMISSION);
+        }
+    }
+
+    public void openCamera() {
+        Toast.makeText(this, "CAMERA PERMISSION GRANTED", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void getContacts() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        startActivityForResult(intent, REQUEST_READ_CONTACT);
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
-            case CAMERA_PERMISSION:
+            case OPEN_CAMERA_PERMISSION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     openCamera();
                 } else {
                     Toast.makeText(this, "CAMERA PERMISSION DENIED", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case READ_CONTACT_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+                    Toast.makeText(this, "READ CONTACTS PERMISSION DENIED", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
@@ -70,22 +113,26 @@ public class MainActivity extends AppCompatActivity {
                     Bitmap imageBitmap = (Bitmap) extras.get("data");
                     ImageView mImageView = findViewById(R.id.imgCapture);
                     mImageView.setImageBitmap(imageBitmap);
+                    mImageView.setVisibility(View.VISIBLE);
                 } else {
                     Toast.makeText(this, "Not able to capture Image", Toast.LENGTH_SHORT).show();
                 }
                 break;
 
+            case REQUEST_READ_CONTACT:
+                if (resultCode == RESULT_OK) {
+                    Uri contactData = data.getData();
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        String number = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Toast.makeText(this, number + " " + name, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                break;
+
 
         }
     }
-
-    public void openCamera() {
-        Toast.makeText(this, "CAMERA PERMISSION GRANTED", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-
 }
