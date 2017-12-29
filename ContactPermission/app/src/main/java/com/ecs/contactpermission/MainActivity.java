0f99be69c95c9472df827c8932ab5d0d36fe226a
manish2rana <manishrana366@gmail.com>
2017-12-29 12:30:14 +0530

    Read & Write Contact Dangerous permission
    Used for runtime permissions related to contacts and profiles on this device.
    1. READ_CONTACTS
    2. WRITE_CONTACTS

--- ContactPermission/app/src/main/java/com/ecs/contactpermission/MainActivity.java
+++ ContactPermission/app/src/main/java/com/ecs/contactpermission/MainActivity.java
@@ -1,22 +1,25 @@
 package com.ecs.contactpermission;
 
 import android.Manifest;
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ecs.adapter.ContactAdapter;
 import com.ecs.pojo.ContactDetail;
@@ -24,18 +27,101 @@ import com.ecs.pojo.ContactDetail;
 import java.util.ArrayList;
 import java.util.List;
 
+import studio.carbonylgroup.textfieldboxes.ExtendedEditText;
+
+import static android.provider.ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+
+
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
 
     public static final int READ_CONTACT_PERMISSION = 101;
     public static final int READ_ALL_CONTACT_PERMISSION = 102;
     public static final int REQUEST_READ_CONTACT = 201;
+    public static final int WRITE_CONTACT_PERMISSION = 103;
+
 
     private ListView lv_contactList;
+
+    private ExtendedEditText eetContactName;
+    private ExtendedEditText eetContactNo;
+    private LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eetContactName = findViewById(R.id.eetContactName);
+        eetContactNo = findViewById(R.id.eetContactNo);
+        layout = findViewById(R.id.layout);
+    }
+
+    public void writeContact(View view) {
+        if (eetContactName.getText().toString().length() > 0 &&
+                eetContactNo.getText().toString().length() == 10) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                getContacts(0);
+                return;
+            }
+
+            int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_CONTACTS);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                boolean flag = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_CONTACTS);
+                if (flag) {
+                    doWriteContact();
+                } else {
+                    showSnackbar("WRITE CONTACTS PERMISSION ALREADY ENABLED");
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_CONTACTS},
+                            WRITE_CONTACT_PERMISSION);
+                }
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CONTACTS}, WRITE_CONTACT_PERMISSION);
+            }
+        } else {
+            showSnackbar("Enter Contact Name/Number");
+        }
+    }
+
+    public void doWriteContact() {
+        boolean success = insertContact(getContentResolver(), eetContactName.getText().toString().trim(), eetContactNo.getText().toString().trim());
+        if (success) {
+            showSnackbar("Contact details Inserted");
+            getContacts(1);         // update contact list
+        } else {
+            showSnackbar("Unable to insert Contact details");
+        }
+    }
+
+    public static boolean insertContact(ContentResolver contactAdder, String firstName, String mobileNumber) {
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI).withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null).withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI).withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0).withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE).withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, firstName).build());
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI).withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0).withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE).withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobileNumber).withValue(ContactsContract.CommonDataKinds.Phone.TYPE, TYPE_MOBILE).build());
+        try {
+            contactAdder.applyBatch(ContactsContract.AUTHORITY, ops);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void showSnackbar(String message) {
+        Snackbar snackbar = Snackbar
+                .make(layout, message, Snackbar.LENGTH_LONG)
+                .setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                });
+
+        snackbar.setActionTextColor(Color.RED);
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(Color.YELLOW);
+        snackbar.show();
     }
 
     public void readSingleContact(View view) {
@@ -49,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
             if (flag) {
                 getContacts(0);
             } else {
-                Toast.makeText(this, "READ CONTACTS PERMISSION ALREADY ENABLED", Toast.LENGTH_SHORT).show();
+                showSnackbar("READ CONTACTS PERMISSION ALREADY ENABLED");
                 ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.READ_CONTACTS},
                         READ_CONTACT_PERMISSION);
@@ -71,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
             if (flag) {
                 getContacts(1);
             } else {
-                Toast.makeText(this, "READ CONTACTS PERMISSION ALREADY ENABLED", Toast.LENGTH_SHORT).show();
+                showSnackbar("READ CONTACTS PERMISSION ALREADY ENABLED");
                 ActivityCompat.requestPermissions(MainActivity.this,
                         new String[]{Manifest.permission.READ_CONTACTS},
                         READ_ALL_CONTACT_PERMISSION);
@@ -135,7 +221,7 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContacts(0);
                 } else {
-                    Toast.makeText(this, "READ CONTACTS PERMISSION DENIED", Toast.LENGTH_SHORT).show();
+                    showSnackbar("READ CONTACTS PERMISSION DENIED");
                 }
                 break;
 
@@ -143,7 +229,15 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContacts(1);
                 } else {
-                    Toast.makeText(this, "READ CONTACTS PERMISSION DENIED", Toast.LENGTH_SHORT).show();
+                    showSnackbar("READ CONTACTS PERMISSION DENIED");
+                }
+                break;
+
+            case WRITE_CONTACT_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doWriteContact();
+                } else {
+                    showSnackbar("WRITE CONTACTS PERMISSION DENIED");
                 }
                 break;
         }
@@ -172,6 +266,4 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-
-
 }
